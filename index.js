f7f7e2bf688bd1e45bb396243d8b121f6031c5fe
Rakesh Das <rakeshdaskowshik@gmail.com>
2016-05-14 21:10:43 -0500

    Testing

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const MLBBoxScores = require('mlbboxscores');
 var http = require ('http');
+var prettify = "https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js";
 //this is a test
 var boxScoresData;
 var options = {
-  path: 'year_2016/month_05/day_13/'
+  path: 'year_2016/month_05/day_14/'
 };
 var mlbboxscores = new MLBBoxScores(options);
 mlbboxscores.get((err, boxscores) => {
   boxScoresData = boxscores;
-  console.log(typeof boxScoresData.toString());
+  // JSON.parse(boxscores);
+  console.log(boxscores);
 });
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
-  res.end("<html>HELLO WORLD!" + "<p>" + JSON.stringify(boxScoresData) + "</p></html>");
+  res.end("<html><script src=" + prettify + "\"></script>" + "<pe class=\"prettyprint\">" + JSON.stringify(boxScoresData) + "</p></html>");
   res.end();
 });
-server.listen(8000);
+server.listen(8100);
