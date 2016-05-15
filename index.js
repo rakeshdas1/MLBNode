233e06bd00e6d89e6e78b7e68ad8641cf2203b20
Rakesh Das <rakeshdaskowshik@gmail.com>
2016-05-14 20:00:31 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const MLBBoxScores = require('mlbboxscores');
+var http = require ('http');
+
+var boxScoresData;
+var options = {
+  path: 'year_2016/month_05/day_13/'
+};
+var mlbboxscores = new MLBBoxScores(options);
+mlbboxscores.get((err, boxscores) => {
+  boxScoresData = boxscores;
+  console.log(typeof boxScoresData.toString());
+});
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end("<html>HELLO WORLD!" + "<p>" + JSON.stringify(boxScoresData) + "</p></html>");
+  res.end();
+});
+server.listen(8000);
